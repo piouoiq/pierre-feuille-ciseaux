9590326e48255cfd615887b7ad7515b1ee999994
astronantes44 <Astronantes@hotmail.fr>
2025-07-24 14:05:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  const choix = ["pierre", "papier", "ciseaux"];
+  const randomIndex = Math.floor(Math.random() * choix.length);
+  return choix[randomIndex];
+}
